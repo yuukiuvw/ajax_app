12b5dbeadef446557724ab668da553cce8c44cb6
yuukiuvw <98954743+yuukiuvw@users.noreply.github.com>
2022-02-17 15:01:55 +0900

    first

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.order(id: "DESC")
+  end
+
+
+ # def new
+ # end
+
+  def create
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
+  end
+  
+end
